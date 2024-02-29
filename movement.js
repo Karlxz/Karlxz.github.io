f6f3b4d1347c4d213e29d6703af853876039c1a7
Karlxz <carloscac1@hotmail.com>
2024-02-28 23:45:40 -0600

    Update movement.js

--- movement.js
+++ movement.js
@@ -17,3 +17,8 @@ async function moveLeft(){
     //app.activeDocument.activeLayer.translate(-10,0);
     await Photopea.runScript(window.parent, "app.activeDocument.activeLayer.translate(-10,0)");
 }
+
+async function setup(){
+    await Photopea.runScript(window.parent, "app.documents.add(800, 700, 72, 'Caramex')");
+    await Photopea.runScript(window.parent, "app.UI.fitTheArea()");
+}
