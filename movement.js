3c1ddf87997b84fe0c8dd6106ad4438a341a6f35
Karlxz <carloscac1@hotmail.com>
2024-02-28 22:43:07 -0600

    mc

--- movement.js
+++ movement.js
@@ -1,15 +1,15 @@
 function moveUp(){
-    app.activeDocument.activeLayer.translate(0,1);
+    app.activeDocument.activeLayer.translate(0,10);
 }
 
 function moveDown(){
-    app.activeDocument.activeLayer.translate(0,-1);
+    app.activeDocument.activeLayer.translate(0,-10);
 }
 
 function moveRight(){
-    app.activeDocument.activeLayer.translate(1,0);
+    app.activeDocument.activeLayer.translate(10,0);
 }
 
 function moveLeft(){
-    app.activeDocument.activeLayer.translate(-1,0);
+    app.activeDocument.activeLayer.translate(-10,0);
 }
