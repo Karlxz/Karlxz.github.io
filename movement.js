cc44b8c8b69bcc02fd6975a42ef29a331a95370e
Karlxz <carloscac1@hotmail.com>
2024-02-28 23:10:38 -0600

    Modifica movement

--- movement.js
+++ movement.js
@@ -1,15 +1,19 @@
-function moveUp(){
-    app.activeDocument.activeLayer.translate(0,10);
+async function moveUp(){
+    //app.activeDocument.activeLayer.translate(0,10);
+    await Photopea.runScript(window.parent, "app.activeDocument.activeLayer.translate(0,10)");
 }
 
-function moveDown(){
-    app.activeDocument.activeLayer.translate(0,-10);
+async function moveDown(){
+    //app.activeDocument.activeLayer.translate(0,-10);
+    await Photopea.runScript(window.parent, "app.activeDocument.activeLayer.translate(0,-10)");
 }
 
-function moveRight(){
-    app.activeDocument.activeLayer.translate(10,0);
+async function moveRight(){
+    //app.activeDocument.activeLayer.translate(10,0);
+    await Photopea.runScript(window.parent, "app.activeDocument.activeLayer.translate(10,0)");
 }
 
-function moveLeft(){
-    app.activeDocument.activeLayer.translate(-10,0);
+async function moveLeft(){
+    //app.activeDocument.activeLayer.translate(-10,0);
+    await Photopea.runScript(window.parent, "app.activeDocument.activeLayer.translate(-10,0)");
 }
