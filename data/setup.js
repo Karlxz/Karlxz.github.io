eaa3f32dad2a20db3208949a9a7231ae0267b506
Karlxz <carloscac1@hotmail.com>
2024-04-24 00:02:46 -0600

    Update setup.js

--- data/setup.js
+++ data/setup.js
@@ -8,19 +8,15 @@ async function addLayer(i, offset, layerName){
     //     addImageAndWait(window.parent, openData(i+offset))
     //         .then(() => Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = "` + layerName + `";`))
     // }
-    var addLayerPromise = new Promise(function(resolve, reject) {
-    
+        var resultado;
         Photopea.runScript(window.parent, `app.activeDocument.layers.getByName("` + layerName + `");`)
             .then(() => Photopea.runScript(window.parent, `app.activeDocument.layers.getByName("` + layerName + `").remove();`))
             .then(() => addImageAndWait(window.parent, openData(i+offset)))
-            .then(() => Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = "` + layerName + `";`))
-            .then(() => resolve())
+            .then(async () => await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = "` + layerName + `";`))
+            .then((result) => resultado = result)
             .catch(() => addImageAndWait(window.parent, openData(i+offset))
-                .then(() => Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = "` + layerName + `";`)))
-                .then(() => resolve())
-    });
-
-    return await addLayerPromise
+                .then(async () => await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = "` + layerName + `";`)))
+                // .then((result) => resultado = result)
     // addImageAndWait(window.parent, openData(i+offset))
     // .then(function(){
     //     Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = ${layerName};`);
